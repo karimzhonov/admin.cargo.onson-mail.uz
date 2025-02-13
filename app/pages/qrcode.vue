68f64262efe57d1adf1b2a689524bcf182fe3334
x.karimjonov <khtkarimzhonov@gmail.com>
2025-02-13 15:09:39 +0500

    test qrcode

--- app/pages/qrcode.vue
+++ app/pages/qrcode.vue
@@ -6,18 +6,48 @@
       style="align-items: center"
     >
       <qr
-        :data="`https://admin.cargo.onson-mail.uz/${localPath('qrcode/')}`"
+        :data="`https://admin.cargo.onson-mail.uz${localPath('/qrcode/')}`"
       />
       <p class="mt-3">
         {{ error }}
       </p>
     </div>
 
-    <qrcode-stream
-      v-else
-      @detect="onDetect"
-      @error="onError"
-    />
+    <div class="flex justify-center">
+      <qrcode-stream
+        :paused="paused"
+        class="qrcode-stream"
+        @detect="onDetect"
+        @error="onError"
+      >
+        <div
+          v-if="validationSuccess"
+          class="validation-success"
+        >
+          <i style="font-size: 30px" class="i-heroicons-check-circle-16-solid text-green-500"></i>
+        </div>
+
+        <div
+          v-else-if="validationFailure"
+          class="validation-failure"
+        >
+          <i style="font-size: 30px" class="i-heroicons-check-circle-16-solid text-red-500"></i>
+        </div>
+
+        <div
+          v-else-if="validationPending"
+          class="validation-pending"
+        >
+          <i style="font-size: 30px" class="i-heroicons-check-circle-16-solid text-green-500"></i>
+        </div>
+        <div
+          v-else
+          class="screen"
+        >
+          <div></div>
+        </div>
+      </qrcode-stream>
+    </div>
     {{ data }}
   </WithNavbar>
 </template>
@@ -29,9 +59,25 @@ export default {
   name: 'Qrcode',
   components: { QrcodeStream },
   data() {
+    console.log(useLocalePath())
     return {
+      paused: false,
       error: null,
-      data: null
+      data: null,
+      isValid: undefined
+    }
+  },
+  computed: {
+    validationPending() {
+      return this.isValid === undefined && this.paused
+    },
+
+    validationSuccess() {
+      return this.isValid === true
+    },
+
+    validationFailure() {
+      return this.isValid === false
     }
   },
   methods: {
@@ -39,9 +85,75 @@ export default {
     onError(error: any) {
       this.error = error
     },
-    onDetect(detectedCodes: any) {
-      this.data = detectedCodes
+    timeout(ms: any) {
+      return new Promise((resolve) => {
+        window.setTimeout(resolve, ms)
+      })
+    },
+    async onDetect([firstDetectedCode]) {
+      this.data = firstDetectedCode.rawValue
+      this.paused = true
+
+      // pretend it's taking really long
+      await this.timeout(1000)
+      this.isValid = this.data.startsWith('https://onson-mail.uz/qrcode/?order_id=')
+
+      // some more delay, so users have time to read the message
+      await this.timeout(1000)
+      this.paused = false
     }
   }
 }
 </script>
+
+<style>
+.qrcode-stream {
+  width: 80%!important;
+  aspect-ratio: 1/1;
+}
+
+.validation-success,
+.validation-failure,
+.validation-pending {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+
+  background-color: rgba(255, 255, 255, 0.8);
+  filter: blur(0.5);
+  padding: 10px;
+  text-align: center;
+  font-weight: bold;
+  font-size: 1.4rem;
+  color: black;
+
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+}
+
+.screen {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+
+  background-color: transparent;
+  padding: 10px;
+  text-align: center;
+  font-weight: bold;
+  font-size: 1.4rem;
+  color: black;
+
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+}
+
+.screen > div {
+  width: 60%;
+  height: 60%;
+  border: 2px solid rgb(34 197 94 / var(--un-text-opacity));
+  border-radius: 5px;
+}
+</style>
